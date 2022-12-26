b094a0aa77fb7e533b980c3805a792d1ea059763
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-26 21:35:23 +0530

    [Akshay]-create Queue by Enqueue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -6,17 +6,23 @@ namespace StackAndQue
     {
         static void Main(string[] args)
         {
-            Stack_s stack_S=new Stack_s();
-            stack_S.push(70);
-            stack_S.push(30);
-            stack_S.push(56);
-            stack_S.Display();
+            //Stack_s stack_S=new Stack_s();
+            //stack_S.push(70);
+            //stack_S.push(30);
+            //stack_S.push(56);
+            //stack_S.Display();
 
-            stack_S.peek();
+            //stack_S.peek();
 
-            stack_S.pop();
+            //stack_S.pop();
 
-            stack_S.Display();
+            //stack_S.Display();
+            Queue_q queue_Q=new Queue_q();
+            queue_Q.Enqueue(56);
+            queue_Q.Enqueue(30);
+            queue_Q.Enqueue(70);
+            queue_Q.display();
+           
         }
     }
 }
---
+++ StackAndQueue/Queue_q.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Queue_q
+    {
+        Node front;
+        Node rear;
+        public void Enqueue(int data)
+        {
+            Node node=new Node(data);
+            if(front==null)
+            {
+                rear= node;
+                front = rear;
+            }
+            else
+            {
+                rear.Next = node;
+                rear = rear.Next;
+            }
+        }
+        public void display()
+        {
+            if(front==null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp= front;
+                while(temp!=null)
+                {
+                    Console.WriteLine("|" + temp.data + "|");
+                    temp = temp.Next;
+                }
+            }
+        }
+       
+    }
+}
