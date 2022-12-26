3fd5255ef5eeca7369addba936ec71607291ed7d
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-26 20:09:33 +0530

    [Akshay]-Create Stack

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackAndQueue;
+
+namespace StackAndQue
+{
+    public class Pogram
+    {
+        static void Main(string[] args)
+        {
+            Stack_s stack_S=new Stack_s();
+            stack_S.push(70);
+            stack_S.push(30);
+            stack_S.push(56);
+            stack_S.Display();
+            
+        }
+    }
+}
---
+++ StackAndQueue/Stack_s.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Stack_s
+    {
+        Node top;
+        public Stack_s()
+        {
+            this.top = null;
+        }
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if(top ==null)
+            {
+                node.Next = null;
+            }
+            else
+            {
+                node.Next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed to stack" , data);
+        }
+        public void Display()
+        {
+            Node temp= this.top;
+            while(temp!= null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.Next;
+            }
+        }
+    }
+}
